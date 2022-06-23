05e004afb67fc936f7a15bed50402cddfe15283f
Maikon Alexandre <maikonalexandre574@gmail.com.br>
2022-06-23 12:43:28 -0300

    23-06-2022

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,10 +1,41 @@
+import { useSession, signIn } from 'next-auth/react';
+import { api } from '../../pages/services/api';
+import { getStripeJs } from '../../pages/services/stripe.js';
 import styles from './styles.module.scss';
 
-export function SubscribeButton(){
+
+
+interface SubscribeButtonProps{
+    priceId: string; 
+}
+export function SubscribeButton({priceId}:SubscribeButtonProps){
+    const { data: session } = useSession()
+
+   async function handleSubscribe(){
+        if(!session){
+            signIn('github')
+            return;
+        }
+        try{
+            const response = await api.post('/subscribe');
+
+            const { sessionId } = response.data;
+      
+            const stripe = await getStripeJs();
+      
+            await stripe.redirectToCheckout({
+              sessionId
+            });
+            
+        }catch(err){
+            alert(err.message);
+        }
+    }
     return(
         <button
         type="button"
         className={styles.subscribeButton}
+        onClick={handleSubscribe}
         >
             Subscribe now
         </button>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,18 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head'
 import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from './services/stripe';
 import styles from './home.module.scss';
+
+interface HomeProps {
+  product:{
+priceId: string,
+amount: number
+  }
+}
  
-export default function Home() {
+export default function Home({product}:HomeProps) {
+
   return (
    <>
    <Head>
@@ -11,14 +21,33 @@ export default function Home() {
    
    <main className={styles.contentContainer}>
     <section className={styles.hero}>
-      <span>👏 Hey, welcome</span>
-      <h1>News about the <span>React</span> world.</h1>
-    <p>Get access to all the publications <br />
-    <span>for $9.90 month</span></p>
-    <SubscribeButton/>
-    </section>
-    <img src="/images/avatar.svg" alt="coding" />
-   </main>
+        <span>👏 Hey, welcome</span>
+        <h1>News about the <span>React</span> world.</h1>
+        <p>Get access to all the publications <br />
+        <span>for {product.amount} month</span></p>
+        <SubscribeButton priceId={product.priceId}/>
+        </section>
+        <img src="/images/avatar.svg" alt="coding" />
+    </main>
    </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async ()=>{
+  const price = await stripe.prices.retrieve('price_1LCDilDxgs4xLq6mgtGAvTHZ')
+
+    const product = {
+      priceId: price.id,
+      amount: new Intl.NumberFormat('en-us', {
+        style: 'currency',
+        currency: 'USD',
+      }).format(price.unit_amount / 100),
+        }
+
+  return{
+    props:{
+      product
+    },
+    revalidate: 60 * 60 * 24, // 24 horas
+  }
 }
