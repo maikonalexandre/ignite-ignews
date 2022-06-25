ff0be7677aaa2e1804df63d586decde34a7c6075
Maikon Alexandre <maikonalexandre574@gmail.com.br>
2022-06-25 18:13:57 -0300

    25-06-2022

---
+++ src/pages/api/_lib/manageSubscription.ts
@@ -0,0 +1,6 @@
+export async function saveSubscription(
+    SubscriptionId: string,
+    custumerId: string
+){
+    
+}
--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,19 +1,52 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import {stripe} from '../services/stripe';
 import { getSession } from "next-auth/react";
+import { fauna } from "../services/fauna";
+import { query as q } from "faunadb";
+
+type User = {
+    ref: {
+        id: string;
+    }
+    data: {
+        stripe_costumer_id: string;
+    }
+}
 
 export default async (req:NextApiRequest, res: NextApiResponse ) => {
     if(req.method === 'POST'){
         const session = await getSession({req})
 
-       const stripeCustomer = await stripe.customers.create({
-        email: session.user.email,
-       })
+        const user = await fauna.query<User>(
+            q.Get(
+                q.Match(
+                    q.Index('user_by_email'),
+                    q.Casefold(session.user.email)
+                )
+            )
+        )
+            let costumerId = user.data.stripe_costumer_id
 
+            if(!costumerId){
+                const stripeCustomer = await stripe.customers.create({
+                    email: session.user.email,
+                   })
 
+                   await fauna.query(
+                    q.Update(
+                        q.Ref(q.Collection('users'), user.ref.id),
+                        {
+                            data: {
+                                stripe_costumer_id: stripeCustomer.id,
+                            }
+                        }
+                    )
+                   )
+                costumerId = stripeCustomer.id;
+            }
 
         const stripeCheckoutSession = await stripe.checkout.sessions.create({
-            customer: stripeCustomer.id,
+            customer: costumerId,
             payment_method_types: ['card'],
             billing_address_collection: 'required',
             line_items: [
