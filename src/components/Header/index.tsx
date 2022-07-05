13f73a004fd9d2621daa3c5885efc86590d50b32
Maikon Alexandre <maikonalexandre574@gmail.com.br>
2022-07-04 23:44:06 -0300

    04-06-2022

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,20 @@
+import { ActiveLink } from "../ActiveLink/ActiveLink";
 import { SignInButton } from "../SignInButton";
 import styles from "./styles.module.scss";
 
 export function Header() {
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="Logo" />
         <nav>
-          <a href="#" className={styles.active}>
-            Home
-          </a>
-          <a href="#">Posts</a>
+          <ActiveLink activeClassName={styles.active } href="/">
+          <a>Home</a>
+          </ActiveLink>
+          <ActiveLink  activeClassName={styles.active } href="/posts" prefetch >
+          <a>Posts</a>
+          </ActiveLink>
         </nav>
         <SignInButton />
       </div>
--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,19 +1,26 @@
 import { useSession, signIn } from "next-auth/react";
+import { useRouter } from "next/router";
 import { api } from "../../services/api";
 import { getStripeJs } from '../../services/stripe.js';
 import styles from "./styles.module.scss";
 
-interface SubscribeButtonProps {
-  priceId: string;
-}
-export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+
+export function SubscribeButton() {
   const { data: session } = useSession();
+  const router = useRouter();
 
   async function handleSubscribe() {
     if (!session) {
       signIn("github");
       return;
     }
+
+    if (session.activeSubscription) {
+      router.push('/posts')
+
+      return;
+    }
+
     try {
       const response = await api.post("/subscribe");
 
